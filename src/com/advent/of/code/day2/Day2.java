e58649846d7092f9d634caadf36bec3cd1507b96
lambatti <adamgrzegorzgala@gmail.com>
2021-12-02 22:10:00 +0100

    The second challenge of Day 2

--- src/com/advent/of/code/day2/Day2.java
+++ src/com/advent/of/code/day2/Day2.java
@@ -9,6 +9,7 @@ public class Day2 {
     public static void challenge1() throws FileNotFoundException {
         File file = new File("src\\com\\advent\\of\\code\\day2\\input.txt");
         Scanner scan = new Scanner(file);
+
         int depth = 0;
         int horizontalPosition = 0;
 
@@ -28,7 +29,35 @@ public class Day2 {
         System.out.println("Horizontal position multiplied by the depth equals to: " + finalPosition);
     }
 
+    public static void challenge2() throws FileNotFoundException {
+        File file = new File("src\\com\\advent\\of\\code\\day2\\input.txt");
+        Scanner scan = new Scanner(file);
+
+        int depth = 0;
+        int horizontalPosition = 0;
+        int aim = 0;
+
+        while(scan.hasNextLine()) {
+            String line = scan.nextLine();
+            int value = Character.getNumericValue(line.charAt(line.length() -1));
+            switch(line.charAt(0)) {
+                case 'f' -> {
+                    horizontalPosition += value;
+                    depth += value*aim;
+                }
+                case 'd' -> aim += value;
+                case 'u' -> aim -= value;
+            }
+        }
+
+        scan.close();
+
+        int finalPosition = horizontalPosition * depth;
+        System.out.println("Horizontal position multiplied by the depth equals to: " + finalPosition);
+    }
+
     public static void main(String[] args) throws FileNotFoundException {
         challenge1();
+        challenge2();
     }
 }
